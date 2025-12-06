843249008f25985b7b8cc42c9bd42ffb35a54ef1
Aaron-Rojas <Arojasolano@gmail.com>
2025-12-06 16:33:56 -0500

    Refactor: App.js
    Se modifico las rutas y se añade dentro CarritoProvider

--- Valdo-Frontend/src/App.js
+++ Valdo-Frontend/src/App.js
@@ -1,28 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Productos from "./pages/Productos";
 import Carrito from "./pages/Carrito";
-
+import {CarritoProvider} from "./context/CarritoContext.jsx";
 
 import "./App.css";
 
 function App() {
-
   return (
-    <BrowserRouter>
-      <Navbar />
 
-      <Routes>
+    <CarritoProvider>
+    <Routes>
+      <Navbar />
         <Route path="/" element={<Home />} />
-        <Route path="/productos" element={<Productos agregar={agregar} />} />
-        <Route path="/carrito" element={<Carrito carrito={carrito} quitar={quitar} />} />
-        <Route path="/mujeres" element={<Mujeres agregar={agregar} />} />
-        <Route path="/hombres" element={<Hombres agregar={agregar} />} />
-        <Route path="/accesorios" element={<Accesorios agregar={agregar} />} />
-        <Route path="/ofertas" element={<Ofertas agregar={agregar} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/carrito" element={<Carrito />} />
+        <Route path="/categoria/:tipo" element={<Productos />} />
+        <Route path="/productos" element={<Productos />} />
       </Routes>
-    </BrowserRouter>
+    </CarritoProvider>
   );
 }
 
