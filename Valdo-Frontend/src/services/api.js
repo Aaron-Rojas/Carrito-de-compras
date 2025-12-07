e70cced25a97827acbad3a7d962b9de96bd02d2d
Aaron-Rojas <Arojasolano@gmail.com>
2025-12-06 21:51:38 -0500

    Deploy final, creo xd

--- Valdo-Frontend/src/services/api.js
+++ Valdo-Frontend/src/services/api.js
@@ -1,6 +1,5 @@
-//const API_URL =  "http://localhost:8080/api";
-const API_URL = import.meta.env.REACT_APP_API_URL ;
-
+//const API_URL =  "http://localhost:8080";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
 export const getHeaders = () => {
     return {
         "Content-Type": "application/json",
